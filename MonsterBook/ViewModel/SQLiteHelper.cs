fb0699dc8ffd83691dee5fc5900075472bb389ab
Trung Kien Huynh <trungkien.huynh@gmail.com>
2020-09-29 13:23:55 +0200

    StartupUri changed to MonsterBookXaml

---
+++ MonsterBook/ViewModel/SQLiteHelper.cs
@@ -0,0 +1,161 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterBook.ViewModel
+{
+    public class SQLiteHelper
+    {
+        public static string dbFile = Path.Combine(Environment.CurrentDirectory, "bomDB.db3");
+
+        public static bool Insert<T>(T item)
+        {
+            bool result = false;
+            using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+            {
+                conn.CreateTable<T>();
+                int numberOfRows = conn.Insert(item);
+                if (numberOfRows < 0)
+                    result = true;
+            }
+            return result;
+        }
+
+        public bool Update<T>(T item)
+        {
+            bool result = false;
+            using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+            {
+                conn.CreateTable<T>();
+                int numberOfRows = conn.Update(item);
+                if (numberOfRows > 0)
+                    result = true;
+            }
+            return result;
+        }
+
+        public bool Delete<T>(T item)
+        {
+            bool result = false;
+            using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+            {
+                conn.CreateTable<T>();
+                int numberOfRows = conn.Delete(item);
+                if (numberOfRows > 0)
+                    result = true;
+            }
+            return result;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
